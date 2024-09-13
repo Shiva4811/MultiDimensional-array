153078f1f160e398fda4ce8d0650dc87f4c78224
Shiv Dev Kaushik <kaushikshiva481@gmail.com>
2024-09-13 18:34:15 +0530

    Next Commit

--- Transpose Of Matrix/TransposeOfMatrix.java
+++ Transpose Of Matrix/TransposeOfMatrix.java
@@ -1,7 +1,9 @@
 class TransposeOfMatrix{
     public static void main(String[] args) {
-        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
-        int[][] tr=new int[3][3];
+        int[][] arr={{1,2,3},{4,5,6}};
+        int m=arr.length;
+        int n=arr[0].length;
+        int[][] tr=new int[n][m];
         System.out.println("The given matrix is: ");
         for(int[] ele: arr){
             for(int x: ele){
@@ -11,14 +13,10 @@ class TransposeOfMatrix{
         }
         System.out.println();
         System.out.println("The transpose of given matrix is: ");
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[0].length; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){ 
                 tr[i][j]=arr[j][i];
-            }
-        }
-        for(int[] ele: tr){
-            for(int x: ele){
-                System.out.print(x+" ");
+                System.out.print(tr[i][j]+" ");
             }
             System.out.println();
         }
